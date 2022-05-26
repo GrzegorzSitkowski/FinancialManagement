862760d122937b6cb16af9d3fae23ef9d4310be9
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-05-26 21:05:13 +0200

    Added entity of Goal

---
+++ FinancialManagment.Domain/Entities/Goals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Domain.Entities
+{
+    public class Goals
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal TargetAmount { get; set; }
+        public decimal SavedAmount { get; set; }
+        public DateTime DesiredDate { get; set; }
+        public string Note { get; set; }
+    }
+}
--- FinancialManagment.Domain/Entities/TransferCategory.cs
+++ FinancialManagment.Domain/Entities/TransferCategory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FinancialManagment.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,8 @@ using System.Threading.Tasks;
 
 namespace FinancialManagment.Domain.Entities
 {
-    public class TransferCategory
+    public class TransferCategory : AuditableEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Transfer> Transfers { get; set; }
     }
