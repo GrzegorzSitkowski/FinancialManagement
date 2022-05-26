dba349cbb51dc888357441047f96384a9d5e7cb3
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-05-26 21:07:40 +0200

    Added entity of GoalCategory

---
+++ FinancialManagment.Domain/Entities/GoalCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Domain.Entities
+{
+    public class GoalCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Goals> Goal { get; set; }
+    }
+}
