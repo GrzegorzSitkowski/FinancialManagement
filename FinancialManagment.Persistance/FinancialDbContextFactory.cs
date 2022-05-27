2ad056062b511ab85eeff5ede61c6bd4c6d5a5cb
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-05-27 21:42:04 +0200

    Added FinancialDbContextFactory

---
+++ FinancialManagment.Persistance/FinancialDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Persistance
+{
+    public class FinancialDbContextFactory : DesignTimeDbContextFactoryBase<FinancialDbContext>
+    {
+        protected override FinancialDbContext CreateNewInstance(DbContextOptions<FinancialDbContext> options)
+        {
+            return new FinancialDbContext(options);
+        }
+    }
+}
