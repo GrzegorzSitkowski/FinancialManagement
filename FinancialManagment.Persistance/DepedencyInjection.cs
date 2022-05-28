dbb82dc66ac07e172b2527ee11640e8eefd6b9e2
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-05-28 13:12:36 +0200

    Created DepedencyInjection in persistance

---
+++ FinancialManagment.Persistance/DepedencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Persistance
+{
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FinancialDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("FinancialDatabase")));
+
+            return services;
+        }
+    }
+}
