7012b5bc3bdc3eac4fe43c19177ca924d7571af0
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-05-28 13:15:53 +0200

    Added Persistance in Startup

--- FinancialManagment.Api/Startup.cs
+++ FinancialManagment.Api/Startup.cs
@@ -1,3 +1,4 @@
+using FinancialManagment.Persistance;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace FinancialManagment.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddPersistance(Configuration);
             services.AddCors(options =>
             options.AddPolicy(name: "MyAllowSpecificOrigins",
             builder =>
