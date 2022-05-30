1c1503dcb8dcf79d95dd6b829f9abfe1da953f09
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-05-30 18:39:29 +0200

    Added AccountConfiguration

---
+++ FinancialManagment.Persistance/Configuration/AccountConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using FinancialManagment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Persistance.Configuration
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.AccountType).IsRequired();
+            builder.Property(p => p.Amount).IsRequired();
+        }
+    }
+}
