2e06a16bdcf27939e755d057d2d97e6e3e7b6f79
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-05-30 18:55:54 +0200

    Added ShoppingListConfiguration

---
+++ FinancialManagment.Persistance/Configuration/ShoppingListConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using FinancialManagment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Persistance.Configuration
+{
+    public class ShoppingListConfiguration : IEntityTypeConfiguration<ShoppingList>
+    {
+        public void Configure(EntityTypeBuilder<ShoppingList> builder)
+        {
+            builder.Property(p => p.Product).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Price).HasDefaultValue(0);
+        }
+    }
+}
