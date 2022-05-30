ff883369ea207bf587fd9d2d24845765de3c0928
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-05-30 18:59:13 +0200

    Added TransferConfiguration

---
+++ FinancialManagment.Persistance/Configuration/TransferConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using FinancialManagment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Persistance.Configuration
+{
+    public class TransferConfiguration : IEntityTypeConfiguration<Transfer>
+    {
+        public void Configure(EntityTypeBuilder<Transfer> builder)
+        {
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Amount).IsRequired();
+            builder.Property(p => p.TransferType).IsRequired();
+            builder.Property(p => p.TransferCategory).IsRequired();
+            builder.Property(p => p.Account).IsRequired();
+        }
+    }
+}
