e4be7affbcc40bd0586d50e05cdb0845ef3092ee
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-05-30 18:54:04 +0200

    Added GoalConfiguration

---
+++ FinancialManagment.Persistance/Configuration/GoalConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using FinancialManagment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Persistance.Configuration
+{
+    public class GoalConfiguration : IEntityTypeConfiguration<Goal>
+    {
+        public void Configure(EntityTypeBuilder<Goal> builder)
+        {
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.GoalCategory).IsRequired();
+            builder.Property(p => p.SavedAmount).HasDefaultValue(0);
+            builder.Property(p => p.TargetAmount).IsRequired();
+        }
+    }
+}
--- FinancialManagment.Persistance/FinancialDbContext.cs
+++ FinancialManagment.Persistance/FinancialDbContext.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -28,6 +29,7 @@ namespace FinancialManagment.Persistance
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             modelBuilder.SeedData();
         }
 
