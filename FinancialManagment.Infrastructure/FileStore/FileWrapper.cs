ac17606f17270bc0234ac1450f4977ed04966537
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-02 20:11:14 +0200

    Added FileWrapper

---
+++ FinancialManagment.Infrastructure/FileStore/FileWrapper.cs
@@ -0,0 +1,18 @@
+﻿using FinancialManagment.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Infrastructure.FileStore
+{
+    public class FileWrapper : IFileWrapper
+    {
+        public void WriteAllBytes(string outputFile, byte[] content)
+        {
+            File.WriteAllBytes(outputFile, content);
+        }
+    }
+}
