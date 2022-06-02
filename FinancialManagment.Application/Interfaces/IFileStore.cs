a9cbba92063cae73f616e65e6de0c8cf57477001
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-02 19:55:37 +0200

    Added IFileStore

---
+++ FinancialManagment.Application/Interfaces/IFileStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Interfaces
+{
+    interface IFileStore
+    {
+        string SafeWriteFile(byte[] content, string sourceFileName, string path);
+    }
+}
