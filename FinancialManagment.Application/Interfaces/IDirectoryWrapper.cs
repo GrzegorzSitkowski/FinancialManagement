7e2c7f1bd7854918246b5c8f889e51979bfeb296
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-02 19:53:00 +0200

    Added IDirectoryWrapper

---
+++ FinancialManagment.Application/Interfaces/IDirectoryWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Interfaces
+{
+    interface IDirectoryWrapper
+    {
+        void CreateDirectory(string path);
+    }
+}
