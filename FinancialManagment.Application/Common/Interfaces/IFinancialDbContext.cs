ca7b9ce667814d939b5c739c84a1093b890f42fe
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-04 13:55:29 +0200

    Created IFinancialDbContext in Application

---
+++ FinancialManagment.Application/Common/Interfaces/IFinancialDbContext.cs
@@ -0,0 +1,25 @@
+﻿using FinancialManagment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Common.Interfaces
+{
+    public interface IFinancialDbContext
+    {
+        DbSet<Account> Accounts { get; set; }
+        DbSet<AccountType> AccountTypes { get; set; }
+        DbSet<Goal> Goals { get; set; }
+        DbSet<GoalCategory> GoalCategories { get; set; }
+        DbSet<ShoppingList> ShoppingLists { get; set; }
+        DbSet<Transfer> Transfers { get; set; }
+        DbSet<TransferCategory> TransferCategories { get; set; }
+        DbSet<TransferType> TransferTypes { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
