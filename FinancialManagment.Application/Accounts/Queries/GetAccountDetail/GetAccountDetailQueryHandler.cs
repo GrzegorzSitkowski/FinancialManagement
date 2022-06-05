bf54dfeae2d42a2d7f7fba9e904a4508eaff95e7
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-05 19:48:37 +0200

    Added Query - GetAccountDetail

---
+++ FinancialManagment.Application/Accounts/Queries/GetAccountDetail/GetAccountDetailQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using FinancialManagment.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Accounts.Queries.GetAccountDetail
+{
+    public class GetAccountDetailQueryHandler : IRequestHandler<GetAccountDetailQuery, AccountDetailVm>
+    {
+        private readonly IFinancialDbContext _context;
+        public GetAccountDetailQueryHandler(IFinancialDbContext financialDbContext)
+        {
+            _context = financialDbContext;
+        }
+
+        public async Task<AccountDetailVm> Handle(GetAccountDetailQuery request, CancellationToken cancellationToken)
+        {
+            var account = await _context.Accounts.Where(p => p.Id == request.AmountId).FirstOrDefaultAsync(cancellationToken);
+
+            var accountVm = new AccountDetailVm()
+            {
+                Name = account.Name,
+                AccountType = account.AccountType,
+                Amount = account.Amount,
+                Transfers = account.Transfers
+            };
+
+            return accountVm;
+        }
+    }
+}
