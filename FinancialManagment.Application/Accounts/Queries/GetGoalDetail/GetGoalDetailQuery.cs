bd2191de6ddaacc0b35ce5389a9e8f8c3f823f26
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-08 15:21:34 +0200

    Added Query - GetGoalDetail

---
+++ FinancialManagment.Application/Accounts/Queries/GetGoalDetail/GetGoalDetailQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Accounts.Queries.GetGoalDetail
+{
+    public class GetGoalDetailQuery : IRequest<GoalDetailVm>
+    {
+        public int GoalId { get; set; }
+    }
+}
---
+++ FinancialManagment.Application/Accounts/Queries/GetGoalDetail/GetGoalDetailQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using FinancialManagment.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Accounts.Queries.GetGoalDetail
+{
+    public class GetGoalDetailQueryHandler : IRequestHandler<GetGoalDetailQuery, GoalDetailVm>
+    {
+        private readonly IFinancialDbContext _context;
+
+        public GetGoalDetailQueryHandler(IFinancialDbContext financialDbContext)
+        {
+            _context = financialDbContext;
+        }
+        public async Task<GoalDetailVm> Handle(GetGoalDetailQuery request, CancellationToken cancellationToken)
+        {
+            var goal = await _context.Goals.Where(p => p.Id == request.GoalId).FirstOrDefaultAsync(cancellationToken);
+
+            var goalVm = new GoalDetailVm()
+            {
+                Name = goal.Name,
+                TargetAmount = goal.TargetAmount,
+                SavedAmount = goal.SavedAmount,
+                DesireDate = goal.DesiredDate,
+                Note = goal.Note,
+                GoalCategorires = goal.GoalCategory
+            };
+
+            return goalVm;
+        }
+    }
+}
