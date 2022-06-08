8a3267e5236760411e417a71e7902ec041d5d5ec
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-08 15:30:07 +0200

    Added Query - GetGoals

---
+++ FinancialManagment.Application/Accounts/Queries/GetGoals/GetGoalsQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using FinancialManagment.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Accounts.Queries.GetGoals
+{
+    public class GetGoalsQueryHandler : IRequestHandler<GetGoalsQuery, GoalsVm>
+    {
+        private readonly IFinancialDbContext _context;
+
+        public GetGoalsQueryHandler(IFinancialDbContext financialDbContext)
+        {
+            _context = financialDbContext;
+        }
+        public async Task<GoalsVm> Handle(GetGoalsQuery request, CancellationToken cancellationToken)
+        {
+            var goals = await _context.Goals.AsNoTracking().ProjectTo<GoalsDto>().ToListAsync();
+
+            return new GoalsVm() { Goals = goals };
+        }
+    }
+}
---
+++ FinancialManagment.Application/Accounts/Queries/GetGoals/GoalsDto.cs
@@ -0,0 +1,17 @@
+﻿using FinancialManagment.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Accounts.Queries.GetGoals
+{
+    public class GoalsDto
+    {
+        public decimal TargetAmount { get; set; }
+        public decimal SavedAmount { get; set; }
+        public DateTime DesiredDate { get; set; }
+        public GoalCategory GoalCategories { get; set; }
+    }
+}
