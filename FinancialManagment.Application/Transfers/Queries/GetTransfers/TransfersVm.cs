b27b9afc61b6176b963cd40304b214c1728c6810
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-08 19:45:46 +0200

    Added Query - GetTransfers

---
+++ FinancialManagment.Application/Transfers/Queries/GetTransfers/TransfersVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Transfers.Queries.GetTransfers
+{
+    public class TransfersVm
+    {
+        public ICollection<TransfersDto> Transfers { get; set; }
+    }
+}
