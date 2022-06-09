ab4feb6b1ed07493c5728749c9b7cf6161bb1872
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-09 18:38:23 +0200

    Added Command - CreateAccount

---
+++ FinancialManagment.Application/Accounts/Commands/CreateAccount/CreateAccountCommand.cs
@@ -0,0 +1,17 @@
+﻿using FinancialManagment.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Accounts.Commands.CreateAccount
+{
+    public class CreateAccountCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public AccountType accountType { get; set; }
+    }
+}
