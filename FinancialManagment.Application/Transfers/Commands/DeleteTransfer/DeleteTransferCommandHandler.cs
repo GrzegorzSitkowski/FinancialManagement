b5f055da348e47aa9110ff0f0235ab14e6d6c14a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-09 21:07:54 +0200

    Added Command - DeleteTransfer

---
+++ FinancialManagment.Application/Transfers/Commands/DeleteTransfer/DeleteTransferCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using FinancialManagment.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Transfers.Commands.DeleteTransfer
+{
+    public class DeleteTransferCommandHandler : IRequestHandler<DeleteTransferCommand>
+    {
+        private readonly IFinancialDbContext _context;
+        public DeleteTransferCommandHandler(IFinancialDbContext financialDbContext)
+        {
+            _context = financialDbContext;
+        }
+
+        public async Task<Unit> Handle(DeleteTransferCommand request, CancellationToken cancellationToken)
+        {
+            var transfer = await _context.Transfers.Where(p => p.Id == request.TransferId).FirstOrDefaultAsync(cancellationToken);
+
+            _context.Transfers.Remove(transfer);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
