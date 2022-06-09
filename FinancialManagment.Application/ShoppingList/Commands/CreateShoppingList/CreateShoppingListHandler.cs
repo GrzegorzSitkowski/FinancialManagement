81294a4b681429a8eddd9363559bc2eab8526be4
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-09 20:05:34 +0200

    Added Command - CreateShoppingList

---
+++ FinancialManagment.Application/ShoppingList/Commands/CreateShoppingList/CreateShoppingListHandler.cs
@@ -0,0 +1,36 @@
+﻿using FinancialManagment.Application.Common.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.ShoppingList.Commands.CreateShoppingList
+{
+    public class CreateShoppingListHandler : IRequestHandler<CreateShoppingListCommand, int>
+    {
+        private readonly IFinancialDbContext _context;
+
+        public CreateShoppingListHandler(IFinancialDbContext financialDbContext)
+        {
+            _context = financialDbContext;
+        }
+
+        public async Task<int> Handle(CreateShoppingListCommand request, CancellationToken cancellationToken)
+        {
+            ShoppingList shoppingList = new()
+            {
+                Product = request.Product,
+                Price = request.Price,
+                Done = request.Done
+            };
+
+            _context.ShoppingLists.Add(shoppingList);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return shoppingList.Id;
+        }
+    }
+}
