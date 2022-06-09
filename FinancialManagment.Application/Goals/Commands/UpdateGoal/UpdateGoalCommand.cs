cce31d2188872d451e7fbc0ace906d0da956329a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-09 19:28:59 +0200

    Added Command - UpdateGoal

---
+++ FinancialManagment.Application/Goals/Commands/UpdateGoal/UpdateGoalCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Goals.Commands.UpdateGoal
+{
+    public class UpdateGoalCommand : IRequest
+    {
+        public int GoalId { get; set; }
+    }
+}
---
+++ FinancialManagment.Application/Goals/Commands/UpdateGoal/UpdateGoalCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using FinancialManagment.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Goals.Commands.UpdateGoal
+{
+    public class UpdateGoalCommandHandler : IRequestHandler<UpdateGoalCommand>
+    {
+        private readonly IFinancialDbContext _context;
+        public UpdateGoalCommandHandler(IFinancialDbContext financialDbContext)
+        {
+            _context = financialDbContext;
+        }
+
+        public async Task<Unit> Handle(UpdateGoalCommand request, CancellationToken cancellationToken)
+        {
+            var goal = await _context.Goals.Where(p => p.Id == request.GoalId).FirstOrDefaultAsync(cancellationToken);
+
+            _context.Goals.Attach(goal);
+            _context.Goals.Update(goal).Property("Name").IsModified = true;
+            _context.Goals.Update(goal).Property("TargetAmount").IsModified = true;
+            _context.Goals.Update(goal).Property("SavedAmount").IsModified = true;
+            _context.Goals.Update(goal).Property("DesiredDate").IsModified = true;
+            _context.Goals.Update(goal).Property("Note").IsModified = true;
+            _context.Goals.Update(goal).Property("goalCategory").IsModified = true;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
