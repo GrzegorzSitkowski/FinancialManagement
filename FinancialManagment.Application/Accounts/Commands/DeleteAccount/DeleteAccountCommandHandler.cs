c67c948d826006e64ad792770bb7f2a8af59723c
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-09 18:47:48 +0200

    Added Command - DeleteAccount

---
+++ FinancialManagment.Application/Accounts/Commands/DeleteAccount/DeleteAccountCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using FinancialManagment.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Accounts.Commands.DeleteAccount
+{
+    public class DeleteAccountCommandHandler : IRequestHandler<DeleteAccountCommand>
+    {
+        private readonly IFinancialDbContext _context;
+
+        public DeleteAccountCommandHandler(IFinancialDbContext financialDbContext)
+        {
+            _context = financialDbContext;
+        }
+
+        public async Task<Unit> Handle(DeleteAccountCommand request, CancellationToken cancellationToken)
+        {
+            var account = await _context.Accounts.Where(p => p.Id == request.AccountId).FirstOrDefaultAsync(cancellationToken);
+
+            _context.Accounts.Remove(account);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
