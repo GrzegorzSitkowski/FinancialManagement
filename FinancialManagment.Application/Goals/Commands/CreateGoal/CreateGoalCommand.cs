864b819f238b6d420c0b8e0a0886f7a45ac832f4
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-09 19:15:24 +0200

    Added Command - CreateGoal

---
+++ FinancialManagment.Application/Goals/Commands/CreateGoal/CreateGoalCommand.cs
@@ -0,0 +1,20 @@
+﻿using FinancialManagment.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Goals.Commands.CreateGoal
+{
+    public class CreateGoalCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public decimal TargetAmount { get; set; }
+        public decimal SavedAmount { get; set; }
+        public DateTime DesiredDate { get; set; }
+        public string Note { get; set; }
+        public GoalCategory goalCategory { get; set; }
+    }
+}
