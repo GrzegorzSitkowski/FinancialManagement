9fbab621704b4d2f071a3a1d605ed25470219952
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-10 19:21:16 +0200

    Installend AutoMapper.Extensions.Microsoft.DepedencyInjection and create DepedencyInjection class in Apllication

---
+++ FinancialManagment.Application/DepedencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using FinancialManagment.Application.Common.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application
+{
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
