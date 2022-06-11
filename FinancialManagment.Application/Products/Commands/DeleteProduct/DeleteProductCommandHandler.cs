7f75353bad3bf262632e71658c99cc0cb31271f6
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-11 16:28:39 +0200

    Mapping in ShoppingList/Products

---
+++ FinancialManagment.Application/Products/Commands/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using FinancialManagment.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Products.Commands.DeleteProduct
+{
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand>
+    {
+        private readonly IFinancialDbContext _context;
+
+        public DeleteProductCommandHandler(IFinancialDbContext financialDbContext)
+        {
+            _context = financialDbContext;
+        }
+
+        public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _context.ShoppingLists.Where(p => p.Id == request.ProductId).FirstOrDefaultAsync(cancellationToken);
+
+            _context.ShoppingLists.Remove(product);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ FinancialManagment.Application/Products/Queries/GetShoppingList/GetShoppingListQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using FinancialManagment.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Products.Queries.GetShoppingList
+{
+    public class GetShoppingListQueryHandler : IRequestHandler<GetShoppingListQuery, ShoppingListVm>
+    {
+        private readonly IFinancialDbContext _context;
+        private readonly IMapper _mapper;
+        public GetShoppingListQueryHandler(IFinancialDbContext financialDbContext, IMapper mapper)
+        {
+            _context = financialDbContext;
+            _mapper = mapper;
+        }
+        public async Task<ShoppingListVm> Handle(GetShoppingListQuery request, CancellationToken cancellationToken)
+        {
+            var shoppingList = await _context.ShoppingLists.AsNoTracking().ProjectTo<ShoppingListDto>(_mapper.ConfigurationProvider).ToListAsync();
+
+            return new ShoppingListVm() { Products = shoppingList };
+        }
+    }
+}
