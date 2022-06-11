416c9dfb88efaf794a10d6ff70bd3275b6691225
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-11 16:34:32 +0200

    Mapping in GetTransfers

--- FinancialManagment.Application/Transfers/Queries/GetTransfers/GetTransfersQueryHandler.cs
+++ FinancialManagment.Application/Transfers/Queries/GetTransfers/GetTransfersQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿using FinancialManagment.Application.Common.Interfaces;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using FinancialManagment.Application.Common.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -13,15 +15,17 @@ namespace FinancialManagment.Application.Transfers.Queries.GetTransfers
     public class GetTransfersQueryHandler : IRequestHandler<GetTransfersQuery, TransfersVm>
     {
         private readonly IFinancialDbContext _context;
+        private readonly IMapper _mapper;
 
-        public GetTransfersQueryHandler(IFinancialDbContext financialDbContext)
+        public GetTransfersQueryHandler(IFinancialDbContext financialDbContext, IMapper mapper)
         {
             _context = financialDbContext;
+            _mapper = mapper;
         }
 
         public async Task<TransfersVm> Handle(GetTransfersQuery request, CancellationToken cancellationToken)
         {
-            var transfers = await _context.Transfers.AsNoTracking().ProjectTo<TransfersDto>().ToListAsync();
+            var transfers = await _context.Transfers.AsNoTracking().ProjectTo<TransfersDto>(_mapper.ConfigurationProvider).ToListAsync();
 
             return new TransfersVm() { Transfers = transfers };
         }
--- FinancialManagment.Application/Transfers/Queries/GetTransfers/TransfersDto.cs
+++ FinancialManagment.Application/Transfers/Queries/GetTransfers/TransfersDto.cs
@@ -1,4 +1,6 @@
-﻿using FinancialManagment.Domain.Entities;
+﻿using AutoMapper;
+using FinancialManagment.Application.Common.Mappings;
+using FinancialManagment.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +9,17 @@ using System.Threading.Tasks;
 
 namespace FinancialManagment.Application.Transfers.Queries.GetTransfers
 {
-    public class TransfersDto
+    public class TransfersDto : IMapFrom<Transfer>
     {
         public decimal Amount { get; set; }
         public TransferType TransferType { get; set; }
         public TransferCategory TransferCategory { get; set; }
         public DateTime Date { get; set; }
         public Account Account { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Transfer, TransfersDto>();
+        }
     }
 }
