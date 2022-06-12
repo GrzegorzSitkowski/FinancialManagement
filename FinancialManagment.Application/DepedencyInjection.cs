8c819c3f068d107935002e9b30d78bd01a25c7de
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-12 18:01:12 +0200

    Added MediatR to DepedencyInjection/Application

--- FinancialManagment.Application/DepedencyInjection.cs
+++ FinancialManagment.Application/DepedencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using FinancialManagment.Application.Common.Interfaces;
+using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -14,6 +15,7 @@ namespace FinancialManagment.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             return services;
         }
