1b553dbb8443e46fcba835b29289ac77ebc22338
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-12 18:40:59 +0200

    Created AccountrController

---
+++ FinancialManagment.Api/Controllers/AccountsController.cs
@@ -0,0 +1,20 @@
+﻿using FinancialManagment.Application.Accounts.Queries.GetAccountDetail;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Api.Controllers
+{
+    [Route("api/accounts")]
+    public class AccountsController : BaseController
+    {
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AccountDetailVm>> GetDetails(int id)
+        {
+            var vm = await Mediator.Send(new GetAccountDetailQuery() { AccountId = id });
+            return vm;
+        }
+    }
+}
