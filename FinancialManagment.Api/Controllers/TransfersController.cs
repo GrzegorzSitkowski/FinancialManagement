7de270d2893293d5547c5a57462fbf5908e8cc7e
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-24 22:04:58 +0200

    Added DeleteTransfer

--- FinancialManagment.Api/Controllers/TransfersController.cs
+++ FinancialManagment.Api/Controllers/TransfersController.cs
@@ -1,5 +1,6 @@
 ﻿using FinancialManagment.Application.Accounts.Queries.GetTransferDetail;
 using FinancialManagment.Application.Transfers.Commands.CreateTransfer;
+using FinancialManagment.Application.Transfers.Commands.DeleteTransfer;
 using FinancialManagment.Application.Transfers.Commands.UpdateTransfer;
 using FinancialManagment.Application.Transfers.Queries.GetTransfers;
 using MediatR;
@@ -38,6 +39,13 @@ namespace FinancialManagment.Api.Controllers
         public async Task<IActionResult> UpdateTransfer(UpdateTransferCommand command)
         {
             var transfer = await Mediator.Send(command);
+            return Ok(transfer);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteTransfer (int id)
+        {
+            var transfer = await Mediator.Send(new DeleteTransferCommand() { TransferId = id });
             return Ok(transfer);
         }
     }
