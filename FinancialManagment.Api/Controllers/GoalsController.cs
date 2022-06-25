4d51eaacb21c47ca874e33ece42e964477756f4d
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-25 17:13:12 +0200

    Created GoalsController

---
+++ FinancialManagment.Api/Controllers/GoalsController.cs
@@ -0,0 +1,15 @@
+﻿using FinancialManagment.Application.Goals.Commands.CreateGoal;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Api.Controllers
+{
+    [Route("api/goals")]
+    public class GoalsController : BaseController
+    {
+       
+    }
+}
