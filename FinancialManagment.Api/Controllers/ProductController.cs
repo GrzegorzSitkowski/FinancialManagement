fd108bc1bf036949484e30823a8c42ecf931885f
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-25 16:29:48 +0200

    Created ProductsController

---
+++ FinancialManagment.Api/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Api.Controllers
+{
+    [Route("api/products")]
+    public class ProductController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
