13f87e4e02a3b8b2f2c9978380184635d5e4f405
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-30 18:03:21 +0200

    Added - DeleteGoalCommandValidator

---
+++ FinancialManagment.Application/Goals/Commands/DeleteGoal/DeleteGoalCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Goals.Commands.DeleteGoal
+{
+    public class DeleteGoalCommandValidator : AbstractValidator<DeleteGoalCommand>
+    {
+        public DeleteGoalCommandValidator()
+        {
+            RuleFor(x => x.GoalId).NotNull();
+        }
+    }
+}
