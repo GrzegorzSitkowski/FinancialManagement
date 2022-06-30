2ae3e36005aca374d5673a726a925bf029268b2a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-30 18:18:29 +0200

    Added - UpdateTransferCommandValidator

---
+++ FinancialManagment.Application/Transfers/Commands/UpdateTransfer/UpdateTransferCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Transfers.Commands.UpdateTransfer
+{
+    public class UpdateTransferCommandValidator : AbstractValidator<UpdateTransferCommand>
+    {
+        public UpdateTransferCommandValidator()
+        {
+            RuleFor(x => x.TransferId).NotNull();
+            RuleFor(x => x.CategoryId).NotNull();
+            RuleFor(x => x.Date).NotNull();
+            RuleFor(x => x.Name).NotNull();
+            RuleFor(x => x.TypeId).NotNull();
+        }
+    }
+}
