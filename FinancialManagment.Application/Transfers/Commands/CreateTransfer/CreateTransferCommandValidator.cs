ac6d4a16374b5492b4cc95beef005726495b21ee
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-30 18:16:01 +0200

    Added - CreateTransferCommandValidator

---
+++ FinancialManagment.Application/Transfers/Commands/CreateTransfer/CreateTransferCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Transfers.Commands.CreateTransfer
+{
+    public class CreateTransferCommandValidator : AbstractValidator<CreateTransferCommand>
+    {
+        public CreateTransferCommandValidator()
+        {
+            RuleFor(x => x.AccountId).NotNull();
+            RuleFor(x => x.Amount).NotNull();
+            RuleFor(x => x.CategoryId).NotNull();
+            RuleFor(x => x.Date).NotNull();
+            RuleFor(x => x.Name).NotNull();
+            RuleFor(x => x.TypeId).NotNull();
+        }
+    }
+}
