ea0a58490830775c6bb858f53e0c9cc9edab2e6d
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-30 17:21:54 +0200

    Configuration PerfomanceBehaviour

--- FinancialManagment.Application/Common/Behaviours/PerformanceBehaviour.cs
+++ FinancialManagment.Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -12,27 +12,32 @@ namespace FinancialManagment.Application.Common.Behaviours
 {
     public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
-        private readonly ILogger _logger;
-        private readonly Stopwatch _timer;
-
-        public PerformanceBehaviour(ILogger<TRequest> logger, Stopwatch timer)
-        {
-            _timer = new Stopwatch();
-            _logger = logger;
-        }
-
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
-        {
-            _timer.Start();
-            var elapsed = _timer.ElapsedMilliseconds;
-
-            if(elapsed > 500)
+            private readonly ILogger _logger;
+            private readonly Stopwatch _timer;
+            public PerformanceBehaviour(ILogger<TRequest> logger)
             {
-                var requestName = typeof(TRequest).Name;
-
-                _logger.LogInformation("FinancialManagment Long Running Request: {Name} ({elapsed} miliseconds) {@Request}",
-                    requestName,elapsed, request);
+                _timer = new Stopwatch();
+                _logger = logger;
             }
-        }
+            public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+            {
+                _timer.Start();
+
+                var response = await next();
+
+                _timer.Stop();
+
+                var elapsed = _timer.ElapsedMilliseconds;
+
+                if (elapsed > 500)
+                {
+                    var requestName = typeof(TRequest).Name;
+
+                    _logger.LogInformation("FinancialManagment Long Running Request: {Name} ({elapsed} milliseconds) {@Request}",
+                     requestName, elapsed, request);
+                }
+
+                return response;
+            }
     }
 }
