971f21fc12bec47e74f0287864cfaf87a80bcea2
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-30 18:06:02 +0200

    Added - UpdateGoalCommandValidator

---
+++ FinancialManagment.Application/Goals/Commands/UpdateGoal/UpdateGoalCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Goals.Commands.UpdateGoal
+{
+    public class UpdateGoalCommandValidator : AbstractValidator<UpdateGoalCommand>
+    {
+        public UpdateGoalCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(30);
+            RuleFor(x => x.GoalId).NotNull();
+            RuleFor(x => x.SavedAmount).NotNull();
+            RuleFor(x => x.TargetAmount).NotNull();
+            RuleFor(x => x.DesireDate).NotNull();
+        }
+    }
+}
