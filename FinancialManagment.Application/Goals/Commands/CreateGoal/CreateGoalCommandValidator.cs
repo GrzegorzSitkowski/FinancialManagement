4f9ad7fff7d6ec88820bf3215f55b3a8b4b830d4
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-30 18:01:41 +0200

    Added - CreateGoalCommandValidator

---
+++ FinancialManagment.Application/Goals/Commands/CreateGoal/CreateGoalCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Goals.Commands.CreateGoal
+{
+    public class CreateGoalCommandValidator : AbstractValidator<CreateGoalCommand>
+    {
+        public CreateGoalCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(30);
+            RuleFor(x => x.SavedAmount).NotNull();
+            RuleFor(x => x.TargetAmount).NotNull();
+            RuleFor(x => x.DesiredDate).NotNull();
+        }
+    }
+}
