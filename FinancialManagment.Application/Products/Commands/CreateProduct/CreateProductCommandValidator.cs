d6d79d9ed1be9ff57dd28438ee0729e8e8c876c3
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-30 18:08:02 +0200

    Added - CreateProductCommandValidator

---
+++ FinancialManagment.Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Products.Commands.CreateProduct
+{
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Product).NotEmpty().MaximumLength(30);
+            RuleFor(x => x.Price).NotNull();
+        }
+    }
+}
