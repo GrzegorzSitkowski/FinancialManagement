e9800d900397321f38c478183b9db7fc1f52b691
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-06-30 17:59:14 +0200

    Added - UpdateAccountCommandValidator

---
+++ FinancialManagment.Application/Accounts/Commands/UpdateAccount/UpdateAccountCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Accounts.Commands.UpdateAccount
+{
+    public class UpdateAccountCommandValidator : AbstractValidator<UpdateAccountCommand>
+    {
+        public UpdateAccountCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(30);
+            RuleFor(x => x.Amount).NotNull();
+        }
+    }
+}
