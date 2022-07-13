534a3fc69d7c17c20bc8c7863d92c9757cb82368
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-07-13 19:07:30 +0200

    Added properties TransferType and TransferCategory in TransferDetailVm

--- FinancialManagment.Application/Transfers/Queries/GetTransferDetail/TransferDetailVm.cs
+++ FinancialManagment.Application/Transfers/Queries/GetTransferDetail/TransferDetailVm.cs
@@ -14,7 +14,9 @@ namespace FinancialManagment.Application.Accounts.Queries.GetTransferDetail
         public string Name { get; set; }
         public decimal Amount { get; set; }
         public int TypeId { get; set; }
+        public TransferType TransferType { get; set; }
         public int CategoryId { get; set; }
+        public TransferCategory TransferCategory { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public int AccountId { get; set; }
