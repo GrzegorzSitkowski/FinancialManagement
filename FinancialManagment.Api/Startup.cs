102bd99e2d1d3eb83b5e89fe7662091b2d468693
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-07-17 17:58:31 +0200

    Configuration Startup to IdentityServer

--- FinancialManagment.Api/Startup.cs
+++ FinancialManagment.Api/Startup.cs
@@ -42,6 +42,19 @@ namespace FinancialManagment.Api
             }));
 
             services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy => policy.AllowAnyOrigin());
+            });
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "https://localhost:5001";
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters();
+                    {
+                        //ValidateAudience = false
+                    };
+                });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FinancialManagment.Api", Version = "v1" });
@@ -49,6 +62,15 @@ namespace FinancialManagment.Api
 
             services.AddDbContext<FinancialDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("FinancialDatabase")));
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "api1");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
