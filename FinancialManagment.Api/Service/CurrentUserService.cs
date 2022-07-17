c23a2f8e9b194ccff52e14f6d7c64b08b9e4b08c
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-07-17 18:08:29 +0200

    Filled CurrentUserService

--- FinancialManagment.Api/Service/CurrentUserService.cs
+++ FinancialManagment.Api/Service/CurrentUserService.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using IdentityModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FinancialManagment.Api.Service
 {
-    public class CurrentUserService
+    public class CurrentUserService : ICurrentUserService
     {
+        public string Email { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            var email = httpContextAccessor.HttpContext?.User?.FindFirstValue(JwtClaimTypes.Email);
+
+            Email = email;
+
+            IsAuthenticated = !string.IsNullOrEmpty(email);
+        }
     }
 }
