ba78672654da673e9cc9f85b4108e2143abccb5b
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-07-20 22:25:07 +0200

    Configuration MappingTestFixture

--- Application.UnitTests/Mapping/MappingTest.cs
+++ Application.UnitTests/Mapping/MappingTest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Application.UnitTests.Mapping
 {
-    class MappingTest
+    public class MappingTest
     {
     }
 }
--- Application.UnitTests/Mapping/MappingTestFixture.cs
+++ Application.UnitTests/Mapping/MappingTestFixture.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoMapper;
+using FinancialManagment.Application.Common.Mappings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace Application.UnitTests.Mapping
 {
-    class MappingTestFixture
+    public class MappingTestFixture
     {
+        public MappingTestFixture()
+        {
+            ConfigurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            Mapper = ConfigurationProvider.CreateMapper();
+        }
+        public IConfigurationProvider ConfigurationProvider { get; set; }
+        public IMapper Mapper { get; set; }
     }
 }
