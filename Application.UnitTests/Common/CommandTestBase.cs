a29dae6da9f93dc5d01142c5da201ae0a2d477cd
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-07-22 21:18:38 +0200

    Added FinancialDbContextFactory

--- Application.UnitTests/Common/CommandTestBase.cs
+++ Application.UnitTests/Common/CommandTestBase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FinancialManagment.Persistance;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace Application.UnitTests.Common
 {
-    class CommandTestBase
+    public class CommandTestBase : IDisposable
     {
+        protected readonly FinancialDbContext _context;
+        protected readonly Mock<FinancialDbContext> _contextMock;
+        public CommandTestBase()
+        {
+            _contextMock = FinancialDbContextFactory.Create();
+            _context = _contextMock.Object;
+        }
+        public void Dispose()
+        {
+            FinancialDbContextFactory.Destroy(_context);
+        }
     }
 }
---
+++ Application.UnitTests/Common/FinancialDbContextFactory.cs
@@ -0,0 +1,80 @@
+﻿using FinancialManagment.Domain.Entities;
+using FinancialManagment.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnitTests.Common
+{
+    public static class FinancialDbContextFactory
+    {
+        public static Mock<FinancialDbContext> Create()
+        {
+            var options = new DbContextOptionsBuilder<FinancialDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            var mock = new Mock<FinancialDbContext>(options) { CallBase = true };
+
+            var context = mock.Object;
+
+            context.Database.EnsureCreated();
+
+            var account = new Account() 
+            { 
+                Id = 9, 
+                StatusId = 1, 
+                Amount = 3000, 
+                TypeId = 1,  
+                Name = "Oszczednosciowe" 
+            };
+            context.Accounts.Add(account);
+
+            var goal = new Goal()
+            {
+                Id = 1,
+                StatusId = 1,
+                CategoryId = 1,
+                Name = "Auto",
+                Note = "Audi",
+                SavedAmount = 1000,
+                TargetAmount = 20000
+            };
+            context.Goals.Add(goal);
+
+            var product = new ShoppingList()
+            {
+                Id = 1,
+                Done = true,
+                Price = 20,
+                StatusId = 1
+            };
+            context.ShoppingLists.Add(product);
+
+            var transfer = new Transfer() 
+            { 
+                AccountId = 1,
+                Amount = 50,
+                CategoryId = 2,
+                Description = "Home",
+                Id = 1, Name = "Lidl",
+                StatusId = 1,
+                TypeId = 2 
+            };
+            context.Transfers.Add(transfer);
+
+            context.SaveChanges();
+
+            return mock;
+        }
+
+        public static void Destroy(FinancialDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
