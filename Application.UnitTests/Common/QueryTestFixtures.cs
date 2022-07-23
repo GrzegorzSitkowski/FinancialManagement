b35780e879c6ae7dfbac1a31015a6bfc3d352677
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-07-23 15:24:44 +0200

    Added QueryTestFixtures

---
+++ Application.UnitTests/Common/QueryTestFixtures.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using FinancialManagment.Application.Common.Mappings;
+using FinancialManagment.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.UnitTests.Common
+{
+    public class QueryTestFixtures : IDisposable
+    {
+        public FinancialDbContext Context { get; private set; }
+        public IMapper Mapper { get; private set; }
+
+        public QueryTestFixtures()
+        {
+            Context = FinancialDbContextFactory.Create().Object;
+
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+            Mapper = configurationProvider.CreateMapper();
+        }
+        public void Dispose()
+        {
+            FinancialDbContextFactory.Destroy(Context);
+        }
+    }
+
+    [CollectionDefinition("QueryCollection")]
+    public class QueryCollection : ICollectionFixture<QueryTestFixtures> { }
+}
