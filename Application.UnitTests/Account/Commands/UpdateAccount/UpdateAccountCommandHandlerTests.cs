9cc47fab6b2b3d4e9f31f9da1b337d1a98f537e6
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-07-24 21:47:30 +0200

    Added test - UpdateAccountCommandHandlerTests

---
+++ Application.UnitTests/Account/Commands/UpdateAccount/UpdateAccountCommandHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using Application.UnitTests.Common;
+using FinancialManagment.Application.Accounts.Commands.UpdateAccount;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.UnitTests.Account.Commands.UpdateAccount
+{
+    public class UpdateAccountCommandHandlerTests : CommandTestBase
+    {
+        private readonly UpdateAccountCommandHandler _handler;
+
+        public UpdateAccountCommandHandlerTests()
+            :base()
+        {
+            _handler = new UpdateAccountCommandHandler(_context);
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidReqest_ShouldDeleteAccount()
+        {
+            var command = new UpdateAccountCommand()
+            {
+                AccountId = 9,
+                Amount = 100,
+                Name = "Test"
+            };
+
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            var acc = await _context.Accounts.FirstAsync(x => x.Id == command.AccountId, CancellationToken.None);
+
+            acc.Name.ShouldBe("Test");
+            acc.Amount.ShouldBe(100);
+        }
+    }
+}
