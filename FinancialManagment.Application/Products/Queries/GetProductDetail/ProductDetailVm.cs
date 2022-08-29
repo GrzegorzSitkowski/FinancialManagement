aec61391064c07dd32498e1ae82501f1fe6f65b4
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-08-29 14:04:33 +0200

    Created GetProductDetailVm

---
+++ FinancialManagment.Application/Products/Queries/GetProductDetail/ProductDetailVm.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using FinancialManagment.Application.Common.Mappings;
+using FinancialManagment.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Products.Queries.GetProductDetail
+{
+    class ProductDetailVm : IMapFrom<ShoppingList>
+    {
+        public string Product { get; set; }
+        public decimal Price { get; set; }
+        public bool Done { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<ShoppingList, ProductDetailVm>();
+        }
+    }
+}
