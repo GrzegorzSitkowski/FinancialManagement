17ba811d037f1ad52d2dc7000caf8b848afbc452
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2022-08-29 14:07:08 +0200

    Created ProductDetailVmQuery

---
+++ FinancialManagment.Application/Products/Queries/GetProductDetail/GetProductDetailQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialManagment.Application.Products.Queries.GetProductDetail
+{
+    public class GetProductDetailQuery : IRequest<ProductDetailVm>
+    {
+        public int Id { get; set; }
+    }
+}
